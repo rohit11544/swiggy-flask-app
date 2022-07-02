624b3a6f9b2c0c9a68f2ca40938f915bb48d2cf8
sandhya <lavurisandhya76@gamil.com>
2022-07-02 14:32:16 +0530

    temp

--- app.py
+++ app.py
@@ -1,30 +1,24 @@
+import os
 from flask import Flask, redirect, request, url_for, render_template
+from dotenv import load_dotenv
+load_dotenv()
+Token = os.getenv("TOKEN")
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def intro1():
-    return "<h1>Hello World</h1>"
+    return "<h1>Hello World </h1>"
 
 
-@app.route('/hello/<user>')
-def intro(user):
-    return render_template('hello.html', name=user)
-
-
-@app.route('/success/<name>')
-def success(name):
-    return "Yo man, we know how to link html and worked with name " + name
-
-
-@app.route('/login', methods=['POST', 'GET'])
+@app.route('/webhook', methods=['POST', 'GET'])
 def login():
-    if request.method == 'POST':
-        user = request.form['nm']
-        return redirect(url_for('success', name=user))
+    if request.method == 'GET':
+        print(request)
+        return "", 200
     else:
-        user = request.args.get('nm')
-        return redirect(url_for('success', name=user))
+        return "", 400
 
 
 if __name__ == '__main__':
