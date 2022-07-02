61e1543be519d59bad1800bb3eda6026dd1c8747
sandhya <lavurisandhya76@gamil.com>
2022-07-02 14:57:56 +0530

    ok

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def login():
     #     return "", 400
     args = request.args
     challenge = args.get("hub.challenge")
-    print("rohittttttttttttttt ----->>>" + request.args)
+    # print("rohittttttttttttttt ----->>>" + request.args)
     return challenge, 200
 
 
