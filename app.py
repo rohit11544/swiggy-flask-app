267943747c6e8d6d37408d3a0197401bd1d22a2f
sandhya <lavurisandhya76@gamil.com>
2022-07-02 16:12:52 +0530

    dd

--- app.py
+++ app.py
@@ -23,10 +23,11 @@ def login():
         if mode and token:
             if mode == "subscribe" and token == Token:
                 print("WEBHOOK_VERIFIED")
-        return "challenge", 400
-    else:
-        return "", 403
-
+                return challenge, 400
+            else:
+                return "", 403
+    if request.method == 'POST':
+        return "worked", 205
 
 if __name__ == '__main__':
     app.run(debug=True)
