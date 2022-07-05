e512309edc5915033a9e30f178f9e9e0f664c3c2
Rohit Borra <rohit.borra@MacBook-Pro-2.local>
2022-07-05 13:29:59 +0530

    audio7

--- helper.py
+++ helper.py
@@ -1,11 +1,11 @@
 import requests
 
-async def download_file(accessToken,media_id):
+def download_file(accessToken,media_id):
     local_filename = media_id + '.ogg'
     headers = {
     'Authorization': 'Bearer '+accessToken,
     }
-    response = await requests.get('https://graph.facebook.com/v13.0/'+media_id, headers=headers)
+    response = requests.get('https://graph.facebook.com/v13.0/'+media_id, headers=headers)
     response_json = response.json()
     url = response_json['url']
     with requests.get(url,headers=headers, stream=True) as r:
